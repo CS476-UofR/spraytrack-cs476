8fcae5e87c58df19057e272886aaff4850bf61f8
Wooyoung <wj100682@gmail.com>
2026-03-29 18:37:34 -0600

    Fix comment formatting in app.js
    fixing my typo

--- Frontend/Assets/app.js
+++ Frontend/Assets/app.js
@@ -150,7 +150,7 @@ function logout(){
 }
 function q(id){ return document.getElementById(id); }
 
-/** Format a record status as a pill badge : woo: Updated to show colour code of status*/\
+/** Format a record status as a pill badge : woo: Updated to show colour code of status*/
   function fmtStatus(status){
   const normalized = String(status || "").toUpperCase();
 
